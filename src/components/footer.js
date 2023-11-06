dec3e68e210ed8d5367060973c318d8f4706958d
Lokteeva <swsw06@mail.ru>
2023-11-06 04:59:13 +0300

    mobile 7

--- src/components/footer.js
+++ src/components/footer.js
@@ -36,6 +36,7 @@ function footer() {
        </div>
        
       </div>
+
       <div>
         <div className="footer__logo2">
           <img src="./image/logo4.png" alt="logo"/>
@@ -45,6 +46,29 @@ function footer() {
         </div>
       </div>
     </div>
+
+    <table class="iksweb">
+	<tbody>
+		<tr>
+			<td colspan="3"><h3>Контакты: Сочи, ул. Фурманова, д. 38</h3></td>
+		</tr>
+		<tr>
+			<td colspan="3"><h3>e-mail: <a href="mailto:ashramsochi@gmail.com"
+            >Ashramsochi@gmail.com</a></h3></td>
+		</tr>
+		<tr>
+			<td> <a href="#"><img width={25} className="footer__whatsapp" src="./image/social/whatsapp-svgrepo-com.svg"></img></a></td>
+			<td><a href="#"><img width={25} className="footer__telegram" src="./image/social/telegram-svgrepo-com.svg"></img></a></td>
+			<td><a href="tel:+79884876527">+7 988 487 6527</a></td>
+		</tr>
+		<tr>
+			<td><a href="#"><img width={25} className="footer__vk" src="./image/social/vk-svgrepo-com.svg"></img></a></td>
+			<td><a href="#"><img width={25} className="footer__instagram" src="./image/social/instagram-167-svgrepo-com.svg"></img></a></td>
+			<td> <a href="#">@ashramsochi</a></td>
+		</tr>
+	</tbody>
+</table>
+
   </footer>
     </div>
   )
