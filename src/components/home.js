ef02052fb2b3936c62a4910ee47eb83a89dd416c
Lokteeva <swsw06@mail.ru>
2023-11-06 07:42:47 +0300

    home 1

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import React from 'react';
 
 function home() {
   return (
     <div>
-      
-    <section id="home" className="home">
+      <section id="home" className="home">
+        <div>
+          <img className="home__img-treeth" src="./image/002.png" alt="home" />
+        </div>
 
-      <img src="./image/home.jpg" alt="home"/>
-    </section>
+        <div>
+          <img className="home__img" src="./image/09.jpg" alt="home" />
+          <div>
+
+            <h3 className="home__button-present">+2 дня отдыха в подарок при оплате в ноябре</h3>
+          </div>
+        </div>
+
+        <div className='home__box'>
+        <div className=" home__date">
+            <h3>29–5</h3>
+            <div className="newYear__month">
+              <button className="newYear__date-dec">декабря</button>
+              <button className="home__date-jun">января</button>
+            </div>
+          </div>
+
+        <div className='home__content'>
+          <div className="home__sochi">
+            <h1>Сочи</h1>
+          </div>
+
+          <h1 className="home__button-festival">
+            Новогодний
+            <br /> фестиваль
+          </h1>
+          <h3 className="home__button-ashram">в ашраме</h3>
+
+          </div>
+
+         
+
+          
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default home
+export default home;
